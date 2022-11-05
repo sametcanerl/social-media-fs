b60a6b800f393f617ca30f2636cef08cc22da751
samet <sametcaneol28@gmail.com>
2022-11-05 15:53:18 +0300

    first commit

---
+++ client/src/components/share/Share.jsx
@@ -0,0 +1,61 @@
+import "./share.scss";
+import Image from "../../assets/img.png";
+import Map from "../../assets/map.png";
+import Friend from "../../assets/friend.png";
+
+import React from "react";
+
+const Share = () => {
+  return (
+    <div className="share">
+      <div className="container">
+        <div className="top">
+          <div className="left">
+            <img src={"/upload/" + currentUser.profilePicture} alt="" />
+            <input
+              type="text"
+              placeholder={`What's on your mind ${currentUser.name}?`}
+              onChange={(e) => setDesc(e.target.value)}
+              value={desc}
+            />
+          </div>
+          <div className="right">
+            {file && (
+              <img className="file" alt="" src={URL.createObjectURL(file)} />
+            )}
+          </div>
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <input
+              type="file"
+              id="file"
+              style={{ display: "none" }}
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+            <label htmlFor="file">
+              <div className="item">
+                <img src={Image} alt="" />
+                <span>Add Image</span>
+              </div>
+            </label>
+            <div className="item">
+              <img src={Map} alt="" />
+              <span>Add Place</span>
+            </div>
+            <div className="item">
+              <img src={Friend} alt="" />
+              <span>Tag Friends</span>
+            </div>
+          </div>
+          <div className="right">
+            <button onClick={handleClick}>Share</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Share;
---
+++ client/src/pages/register/Register.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./register.scss";
+import axios from "axios";
+
+const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+    name: "",
+  });
+  const [err, setErr] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputs((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:8800/api/auth/register", inputs);
+    } catch (err) {
+      setErr(err.response.data);
+    }
+  };
+ 
+
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>SCE SOCİAL.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque sit
+            omnis et impedit labore velit animi libero dolorem incidunt
+            similique?
+          </p>
+          <span>Do you have an account</span>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              onChange={handleChange}
+            />
+            {err && err}
+            <button onClick={handleClick}>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
